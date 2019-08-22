b2506f1b6e0d039febf2bc2cd66dc550b463b4fc
Jason Requizo <jason.req@gmail.com>
2019-08-23 04:41:43 +1000

    Updates

---
+++ precache-manifest.e2f859685a812131567acc7e8ae50be0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9424702af8cbc6a0b233bb3e35621ae6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aa5e7dcb7e3b4e935d0f",
+    "url": "/portfolio/static/css/main.bc56328d.chunk.css"
+  },
+  {
+    "revision": "3ca3934f16e4b392605d",
+    "url": "/portfolio/static/js/2.7053c2b5.chunk.js"
+  },
+  {
+    "revision": "aa5e7dcb7e3b4e935d0f",
+    "url": "/portfolio/static/js/main.8e07a916.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
